cee980e81e7303ca310b2529dc67ce47cb7a14b0
Uzum Stanley <70582317+uzumstanley@users.noreply.github.com>
2024-12-28 21:31:18 +0000

    Update MultipleLinearRegression3dcode-1 - app.py

--- MultipleLinearRegression3dcode-1 - app.py
+++ MultipleLinearRegression3dcode-1 - app.py
@@ -49,7 +49,7 @@ print('Coefficient of determination: %.2f'
 
 
 # Save the trained model to a file using Pickle
-with open('model.pkl', 'wb') as file:  # 'wb' means write in binary mode
+with open('Muliplelinear_regression_model.pkl', 'wb') as file:  # 'wb' means write in binary mode
     pickle.dump(regr, file)
 
 
